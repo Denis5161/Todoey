b04682d63e5cdf5ae962a8e894f98c61e96ce5dd
Denis Goldberg <dnsgoldberg6@gmail.com>
2018-09-02 23:16:02 +0200

    Convert to MVC design pattern.

---
+++ Todoey/Data Model/Item.swift
@@ -0,0 +1,15 @@
+//
+//  Item.swift
+//  Todoey
+//
+//  Created by Denis Goldberg on 02.09.18.
+//  Copyright © 2018 Denis Goldberg. All rights reserved.
+//
+
+import Foundation
+
+class Item {
+    
+    var title : String = ""
+    var done : Bool = false
+}
